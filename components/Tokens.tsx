850ea34bbb509ffe0bb9b540a06e95531e77df92
anhoangphuc <hoangphucnb97@gmail.com>
2024-12-02 15:53:12 +0700

    Add link to to address of token

--- components/Tokens.tsx
+++ components/Tokens.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image';
 import { getJupiterApiClient } from '@/utils/jupiterApiClient';
 import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
-import { TrashIcon } from '@heroicons/react/24/outline';
+import { TrashIcon, ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline';
 import { createCloseAccountInstruction } from '@solana/spl-token';
 import { Transaction } from '@solana/web3.js';
 import toast, { Toaster } from 'react-hot-toast';
@@ -490,6 +490,18 @@ const Tokens = () => {
                                                             Delete Token Account and redeem SOL
                                                         </span>
                                                     </button>
+
+                                                    <a
+                                                        href={getAddressLink(token.mint)}
+                                                        target="_blank"
+                                                        rel="noopener noreferrer"
+                                                        className="p-2 hover:bg-blue-900/20 rounded-full transition-colors group relative"
+                                                    >
+                                                        <ArrowTopRightOnSquareIcon className="w-5 h-5 text-blue-400 hover:text-blue-300" />
+                                                        <span className="absolute -top-14 left-1/2 transform -translate-x-1/2 bg-gray-900 text-white text-sm px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap z-10 pointer-events-none">
+                                                            View on Solana Explorer
+                                                        </span>
+                                                    </a>
 
                                                     <input
                                                         type="checkbox"
